93a110482c30e7d24a3c430745d22cf44561b1fc
Rayssa <rayssarrsilva1@gmail.com>
2025-12-03 16:02:33 -0300

    criação de env pra uso de docker na maquina 1 + ajustes nos imports do backend

--- backend/security.py
+++ backend/security.py
@@ -3,8 +3,8 @@ from jose import JWTError, jwt
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-from backend.models import User
-from backend.database import get_db
+from models import User
+from database import get_db
 import os
 from dotenv import load_dotenv
 
